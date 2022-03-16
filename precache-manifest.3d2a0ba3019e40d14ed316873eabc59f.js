63432628f2ed8a8ae288f0c10e71b6e3db309a88
zargouniwassim <79200921+zargouniwassim@users.noreply.github.com>
2022-03-16 06:34:50 +0100

    hela

---
+++ precache-manifest.3d2a0ba3019e40d14ed316873eabc59f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651f881abc3ca7631b530a5bcb99206c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38d5c7bb2b55b2837708",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "16b9a720b00922099ae3",
+    "url": "/static/css/main.b50dfe6d.chunk.css"
+  },
+  {
+    "revision": "38d5c7bb2b55b2837708",
+    "url": "/static/js/2.0b9e910f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0b9e910f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16b9a720b00922099ae3",
+    "url": "/static/js/main.6b59302d.chunk.js"
+  },
+  {
+    "revision": "f5e337750815ed0ba0e7",
+    "url": "/static/js/runtime-main.a0044592.js"
+  }
+]);
